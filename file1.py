460947134b5f2d6ac2403c8666e54fe5c6314017
Aleem24 <md.samiulaleem16@gmail.com>
2025-03-08 15:38:35 +0600

    I ahve done my Acp on operation,Alhamdulillah

--- file1.py
+++ file1.py
@@ -1,31 +1,31 @@
-
-file1 = open("new_file.txt","r")
-# print(file1.read(4))#the argument inside read function denotes the characters inside the file.
+#the argument inside read function denotes the characters inside the file. 
+file1 = open("operate.txt","r")
+print(file1.read(20))
 file1.close()
 
-file2 = open("new_file.txt","r")
-# print(file2.readline())
+file2 = open("operate.txt","r")
+print(file2.readline())
 file2.close()
 
-file3 = open("new_file.txt", "r")
+file3 = open("operate.txt", "r")
 
-# for x in file3:
-#     print(x)
+for x in file3:
+    print(x)
 
 file3.close()
 
-file4 = open("new_file.txt","r")
-# print(file4.readlines())
+file4 = open("operate.txt","r")
+print(file4.readlines())
 file4.close()
 
 #removing lines from file
 
-file5 = open("new_file.txt","r")
-file6 = open("updated_file.txt","w")
+file5 = open("operate.txt","r")
+file6 = open("updated.txt","w")
 
 for line in file5.readlines():
-    if line.startswith("We"):
-        # print(line)
+    if line.startswith("Then"):
+        print(line)
         file6.write(line)
 
     
@@ -33,8 +33,8 @@ for line in file5.readlines():
 file5.close()
 file6.close()
 
-file7 = open("new_file.txt","r")
-file8 = open("fresh_file.txt","w")
+file7 = open("operate.txt","r")
+file8 = open("fresh.txt","w")
 
 contents = file7.readlines()
 print(type(contents))
